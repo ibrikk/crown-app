c91cc8ccb9f8bcb87026ff1d18df04906d765fdf
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-MacBook-Pro.local>
2020-04-19 18:19:59 -0400

    adding header component and links to home, shoppage and contact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
-  <div>
-  
-  <Switch> 
-  <Route exact path='/' component ={HomePage} />
-  <Route path='/shop' component ={ShopPage} />
-  </Switch>
-  
-  </div>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
   );
-  }
+}
 
-  
 export default App;
