6f48e56348b493f1b474fb8a6aaa97e81479cb2c
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-MacBook-Pro.local>
2020-05-03 16:42:21 -0400

    updating our app to redirect to home if user is signed in

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,6 @@
+import { UserActionTypes } from './user.types';
+
 export const setCurrentUser = user => ({
-    type: 'SET_CURRENT_USER', // STRING HAS TO BE SAME AS IN user.reducer.js
+    type: UserActionTypes.SET_CURRENT_USER, // STRING HAS TO BE SAME AS IN user.reducer.js
     payload: user 
 });
