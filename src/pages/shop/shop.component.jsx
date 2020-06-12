b9cea1c1561f4c5fac80134fa99683f55bfa548c
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-MacBook-Pro.local>
2020-06-11 23:20:37 -0400

    updated our application app component, sign-in component and sign-up component to use hooks

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -8,15 +8,11 @@ import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 import CollectionsOverviewContainer from '../../components/collection-overview/collections-overview.container';
 
 
-class ShopPage extends React.Component {
- 
-  componentDidMount() {
-   const { fetchCollectionsStart } = this.props;
-   fetchCollectionsStart();
-  }
-
-  render() {
-    const { match } = this.props;
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+ useEffect(() => {
+   fetchCollectionsStart()
+ }, [fetchCollectionsStart]);
+  
     return (
       <div className='shop-page'>
         <Route
@@ -30,8 +26,8 @@ class ShopPage extends React.Component {
         />
       </div>
     );
-  }
-}
+    }
+
 
 
 
